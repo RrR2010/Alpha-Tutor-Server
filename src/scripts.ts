d0cd41fef47cb78d863b0ec03641a72e79a3018f
RrR2010 <richard.rsmi@hotmail.com>
2024-07-19 03:12:23 -0300

    added function for importing random user test data

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,61 +1,31 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+import { importUsers } from '../tests/db/randomData'
 
 const prisma = new PrismaClient()
 
 export async function main() {
   const activeStatus = await prisma.userStatus.upsert({
-    where: {name: 'Ativo'},
+    where: { name: 'Ativo' },
     update: {},
-    create: {name: 'Ativo'},
+    create: { name: 'Ativo' }
   })
   const inactiveStatus = await prisma.userStatus.upsert({
-    where: {name: 'Inativo'},
+    where: { name: 'Inativo' },
     update: {},
-    create: {name: 'Inativo'},
+    create: { name: 'Inativo' }
   })
   const vacationStatus = await prisma.userStatus.upsert({
-    where: {name: 'Férias'},
+    where: { name: 'Férias' },
     update: {},
-    create: {name: 'Férias'},
+    create: { name: 'Férias' }
   })
   const sickLeaveStatus = await prisma.userStatus.upsert({
-    where: {name: 'Afastado'},
+    where: { name: 'Afastado' },
     update: {},
-    create: {name: 'Afastado'},
+    create: { name: 'Afastado' }
   })
 
-  const user = await prisma.user.upsert({
-    where: {externalID: '555314'},
-    update: {},
-    create: {
-      externalID: '555314',
-      name: 'Richard de Souza Morais Inacio',
-      email: 'rdinacio@arcor.com',
-      statusId: activeStatus.id,
-    },
-  })
-
-  const richardUser = await prisma.user.findFirstOrThrow({where: {email: 'rdinacio@arcor.com'}})
-
-  const adminRole = await prisma.role.upsert({
-    where: {name: 'Admin'},
-    update: {},
-    create: {
-      name: 'Admin',
-      description:
-        'Tem acesso completo ao sistema, incluindo gestão de usuários, configuração da plataforma, gerenciamento de conteúdo, segurança, e acesso a relatórios detalhados.',
-    },
-  })
-
-  const userRole = await prisma.userRole.upsert({
-    where: {userId_roleId: {userId: richardUser.id, roleId: adminRole.id}},
-    update: {},
-    create: {
-      userId: richardUser.id,
-      roleId: adminRole.id,
-      assignedById: richardUser.id,
-    },
-  })
+  await importUsers()
 }
 
 main()
---
+++ tests/db/randomData.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client'
+import userJson from './randomData/user.json'
+
+const prisma = new PrismaClient()
+
+export async function importUsers() {
+  userJson.forEach(async (user) => {
+    const status = await prisma.userStatus.upsert({
+      where: { name: user.statusName },
+      update: {},
+      create: {
+        name: user.statusName
+      }
+    })
+
+    await prisma.user.upsert({
+      where: { externalID: user.externalID },
+      update: {},
+      create: {
+        externalID: user.externalID,
+        name: user.name,
+        email: user.email,
+        statusId: status.id
+      }
+    })
+  })
+}
